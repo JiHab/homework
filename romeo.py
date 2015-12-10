924d72d8e05428ab46d0c6fd7659942edba27658
jihab <vladimir.mail@ukr.net>
2015-12-11 00:26:20 +0200

    hw 11.12.2015

---
+++ romeo.py
@@ -0,0 +1,23 @@
+f = open('C:\\Users\\ACER\\Documents\\romeo.txt')
+l2=[]
+for line in f:
+    l2 = l2 + line.replace('\n','').split(' ')
+l2.sort()
+print(l2)
+
+for line in f:
+    word =
+
+print(l2)
+
+#f2= open('C:\\Users\\ACER\\Documents\\romeo.txt').read()
+#print(f2.read())
+#for word in f2:
+#    print(word)
+#
+
+    #print(line)
+    #print(line.replace('\n',' '))
+
+    #line.split(' ')
+    #print(line.split(' '))
