ff1e0b690100408ed49a1fd60b45d5561d615889
jihab <vladimir.mail@ukr.net>
2015-12-11 01:09:18 +0200

    hw_2 11.12.2015

--- romeo.py
+++ romeo.py
@@ -5,11 +5,15 @@ for line in f:
 l2.sort()
 print(l2)
 
-for line in f:
-    word =
+l_or = []
+l_or = [c for c in l2 if c not in l_or]
+#for word in l2:
+ #   if word in l_or:
+  #      q = 1+2
+   # else:
+    #    l_or.append(word)
 
-print(l2)
-
+print(l_or)
 #f2= open('C:\\Users\\ACER\\Documents\\romeo.txt').read()
 #print(f2.read())
 #for word in f2:
